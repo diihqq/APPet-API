0b45c585c2e321251e7032210d59bfd98c5aeac4
albertzz <alephert@outlook.com>
2016-08-20 22:33:33 -0300

    listaRacas e listaRacaPorEspecies
    utf8_encode para retornar registros com acento

--- raca.php
+++ raca.php
@@ -6,22 +6,48 @@ function ListaRacas($id){
 
 	$id = mysqli_real_escape_string($conexao,$id);
 	
-	//Consulta raça no banco
+	//Consulta raca no banco
 	if($id == 0){
-		$query = mysqli_query($conexao,"SELECT * FROM Raca") or die(mysqli_error($conexao));
+		$query = mysqli_query($conexao,"SELECT idRaca, Nome, Descricao, idEspecie FROM Raca") or die(mysqli_error($conexao));
 	}else{
-		$query = mysqli_query($conexao,"SELECT * FROM Raca WHERE idRaca = " .$id) or die(mysqli_error($conexao));
+		$query = mysqli_query($conexao,"SELECT idRaca, Nome, Descricao, idEspecie FROM Raca WHERE idRaca = " .$id) or die(mysqli_error($conexao));
 	}
+	
 	//faz um looping e cria um array com os campos da consulta
 	while($dados = mysqli_fetch_array($query))
 	{
 		$resposta[] = array('idRaca' => $dados['idRaca'],
-							'Nome' => $dados['Nome'],
-							'Descricao' => $dados['Descricao'],
+							'Nome' => utf8_encode($dados['Nome']),
+							'Descricao' => utf8_encode($dados['Descricao']),
 							'idEspecie' => $dados['idEspecie']);
 	}
 	
 	return $resposta;
 }
 
+function ListaRacasPorEspecie($id){
+	include("conectar.php");
+	
+	$resposta = array();
+
+	$id = mysqli_real_escape_string($conexao,$id);
+	
+	//Consulta raca no banco
+	if($id == 0){
+		$resposta = mensagens(2);
+	}else{
+		$query = mysqli_query($conexao,"SELECT idRaca, Nome, Descricao, idEspecie FROM Raca WHERE idEspecie = " .$id) or die(mysqli_error($conexao));
+	}
+	
+	//faz um looping e cria um array com os campos da consulta
+	while($dados = mysqli_fetch_array($query))
+	{
+		$resposta[] = array('idRaca' => $dados['idRaca'],
+							'Nome' => utf8_encode($dados['Nome']),
+							'Descricao' => utf8_encode($dados['Descricao']),
+							'idEspecie' => $dados['idEspecie']);
+	}
+	
+	return $resposta;
+}
 ?>
